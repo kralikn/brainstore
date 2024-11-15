16bfca50d782e32941e26eb1289b7addd221aa07
Králik Norbert <kraaliknorbert@gmail.com>
2024-11-15 02:58:43 +0100

    chat skeleton

--- components/loading-chat-file-list.js
+++ components/loading-chat-file-list.js
@@ -2,7 +2,7 @@ import { Skeleton } from "./ui/skeleton";
 
 export default function LoadingChatFileList() {
   return (
-    <div className='grid grid-cols-12 min-h-[calc(100vh-20rem)] gap-4'>
+    <div className='grid grid-cols-12 min-h-[calc(100vh-30rem)] gap-4'>
       <div className='col-span-12 h-full space-y-3'>
         <div className="h-10">
           <Skeleton className="h-full w-full rounded-md bg-gray-200" />
