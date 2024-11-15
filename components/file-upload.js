efb569002502cb107511c82937a381488eb3048b
Králik Norbert <kraaliknorbert@gmail.com>
2024-11-15 01:28:37 +0100

    skeleton topics list page header

--- components/file-upload.js
+++ components/file-upload.js
@@ -12,14 +12,14 @@ import LoadingDocsListHeader from './loading-docs-list-header'
 
 export default function FileUpload({ topicSlug }) {
 
-  const { data, isLoading, isPending } = useQuery({
+  const { data, isLoading } = useQuery({
     queryKey: ['topic', topicSlug],
     queryFn: () => getFiles(topicSlug),
   })
 
   const topicTitle = data?.topicTitle || ""
 
-  if (isLoading || isPending) return <LoadingDocsListHeader />
+  if (isLoading) return <LoadingDocsListHeader />
 
   return (
     <Card className="bg-gray-50 border-none flex justify-between items-end">
